a058fb848c3265a06b277d09c4f0d1f3e057bce4
samith18 <157686005+samith18@users.noreply.github.com>
2024-02-07 21:39:24 +0530

    Create LT2_1.c

---
+++ LT2_1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+  //Write a program in C to store elements in an array and print them.
+	int n,i;
+	printf("enter the size of array:");
+	int arr[n];
+	scanf("%d",&n);
+	printf("enter the elements of array:");
+	for(i=0;i<n;i++)
+	{
+	
+	scanf("%d",&arr[i]);
+}
+	printf("enter the elements in  array");
+	for(i=0;i<n;i++)
+	{
+	
+	printf("%d",arr[i]);
+}
+printf("\n");
+     return 0;
+}
